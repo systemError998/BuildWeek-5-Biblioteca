2c25278d5ecf434b48c01f9c61bd1d756d021064
VczEno <arpaia.vin@gmail.com>
2024-03-22 16:04:37 +0100

    altri componentini (HR caro non so chi sia tale Gregorio Vecchio)

--- biblieria/src/Components/BookDetailComp/BookDetailComponent.jsx
+++ biblieria/src/Components/BookDetailComp/BookDetailComponent.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function BookDetailComponent() {
   return (
-    <div>BookDetailComponent</div>
+    <div className="container bg-dark rounded-2 text-light p-5">
+        <div className="row">
+            <div className="col">
+                <div className='border-1 border-light rounded-1'>
+                    IMG
+                </div>
+            </div>
+            <div className="col">
+                <h1>Titolo Libro</h1>
+                <h3>Autore</h3>
+                <div className='my-3'>
+                    <p className='m-0'><b>Anno pubblicazione: </b> <span className='text-secondary'>data</span></p>
+                    <p className='m-0'><b>Categoria: </b> <span className='text-secondary'>categoria</span></p>
+                    <p className='m-0'><b>Numero di pagine: </b> <span className='text-secondary'>200</span></p>
+                    <p className='m-0'><b>Copie disponibili: </b> <span className='text-secondary'>4</span></p>
+                </div>
+                <div className='my-4'>
+                    Trama...
+                </div>
+            </div>
+        </div>
+        <div className="row text-center">
+            <div className="col">
+                <button className='btn btn-light w-75'>PRENOTA</button>
+            </div>
+        </div>
+    </div>
   )
 }
--- biblieria/src/Components/ProfilePageComp/ProfileDetail.jsx
+++ biblieria/src/Components/ProfilePageComp/ProfileDetail.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function ProfileDetail() {
   return (
-    <div>ProfileDetail</div>
+    <div className="container bg-dark rounded-2 text-light p-5">
+        <div className="row d-flex justify-content-center">
+            <div className="col d-flex justify-content-center">
+                <div className='border-1 border-light text-center rounded-circle w-100' style={{height: "13em"}}>
+                    IMG
+                </div>
+            </div>
+            <div className="col d-flex justify-content-between">
+                <div className=''>
+                    <h1>Nome</h1>
+                    <p><b>Email: </b> <span className="text-secondary">example@example.com</span></p>
+                    <button className='btn btn-light'>Modifica i tuoi dati</button>
+                </div>
+            </div>
+        </div>
+        <div className="row gap-3 mt-4">
+            <h3>Preferiti</h3>
+            <div className="col border-1 border-light rounded-1" style={{height: "10em"}}>
+                librone
+            </div>
+            <div className="col border-1 border-light rounded-1" style={{height: "10em"}}>
+                librone
+            </div>
+            <div className="col border-1 border-light rounded-1" style={{height: "10em"}}>
+                librone
+            </div>
+            <div className="col border-1 border-light rounded-1" style={{height: "10em"}}>
+                librone
+            </div>
+        </div>
+    </div>
   )
 }
