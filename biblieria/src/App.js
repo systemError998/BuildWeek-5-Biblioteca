d85bcbeec59bb1e8be98bd57541f72cc4fb45737
systemError998 <annacerasoli998@gmail.com>
2024-03-22 11:51:41 +0100

    navbar

--- biblieria/src/App.js
+++ biblieria/src/App.js
@@ -1,19 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from './Pages/HomePage';
 import NotFoundPage from './Pages/NotFoundPage';
 import ProfilePage from './Pages/ProfilePage';
+import MyNavbar from './Components/MyNavbar';
 
 
 function App() {
   return (
     <BrowserRouter scrollToTop={true}>
-
+      <MyNavbar />
     {/*   <Scrollata /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />  
-        <Route path="/dashboard" element={< ProfilePage />} />
         <Route path="*" element={< NotFoundPage />} />
       
       </Routes>
