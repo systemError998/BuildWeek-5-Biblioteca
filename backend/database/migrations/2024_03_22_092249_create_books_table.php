3bf26f34853d7bc6a9c0ff5954580f9e234fd1d7
systemError998 <annacerasoli998@gmail.com>
2024-03-25 21:52:45 +0100

    il mio nome è eastwood, CLINT EASTWOOD

--- backend/database/migrations/2024_03_22_092249_create_books_table.php
+++ backend/database/migrations/2024_03_22_092249_create_books_table.php
@@ -14,19 +14,19 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-           
+
             $table->string("title");
             $table->year('year');
             $table->integer("available_copies")->default(1);
             $table->string("cover_url")
             ->default("https://i.pinimg.com/736x/b5/d7/ed/b5d7edaf6d61091281f348fb1e66efc8.jpg");
             $table->text("abstract");
-            $table->foreignId('author_id');            
+            $table->foreignId('author_id');
             $table->foreign('author_id')->on('authors')->references('id')
-            ->onDelete('cascade')->onUpdate('cascade'); 
+            ->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('category_id');
             $table->foreign('category_id')->on('categories')->references('id')
-            ->onDelete('cascade')->onUpdate('cascade');   
+            ->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
