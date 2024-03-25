e652adb84b84a04dad596d448c816845979ab84a
AleDC91 <ale91.dallas@gmail.com>
2024-03-25 19:00:05 +0100

    bastardolaltissimo

--- backend/database/migrations/2024_03_25_101904_create_favorites_table.php
+++ backend/database/migrations/2024_03_25_101904_create_favorites_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Favorites', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id");
             $table->foreign("user_id")->on("users")->references("id")->onDelete("cascade")->onUpdate("cascade");
