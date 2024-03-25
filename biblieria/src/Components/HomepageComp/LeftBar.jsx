3fc77c0845813783c141d487aa0e350622ed9118
systemError998 <annacerasoli998@gmail.com>
2024-03-25 18:47:05 +0100

    "ti pusho l'anima"

--- biblieria/src/Components/HomepageComp/LeftBar.jsx
+++ biblieria/src/Components/HomepageComp/LeftBar.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { getCategorie } from '../../slice/categorySlice.js';
 import "../../assets/css/Categories.css"
 import "../../assets/script/Anna.js"
+import { selezionaCategoria } from '../../slice/bookSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllBooks } from '../../slice/bookSlice';
 
 import LeftBarList from './LeftBarList.jsx';
 
@@ -12,6 +14,7 @@ export default function LeftBar() {
     const dispatch = useDispatch()
 
     const category= useSelector(state=>state.categorie.listaCategorie)
+    const categoriaSelezionata = useSelector(state => state.libri.categoriaSelezionata)
 
     useEffect(()=>{
         dispatch(getCategorie())
@@ -26,6 +29,13 @@ return (
         <div className="navigation mt-4 rounded-md">
             <p className='uppercase font-sans font-semibold border-b-2 pb-2'> Categorie </p>
             <ul className="uppercase font-sans font-semibold text-center">
+            <li className={"list text-center " + categoriaSelezionata == null ? 'active' : ''} >
+                <button className="flex gap-1 no-underline uppercase "  onClick={() => { dispatch(getAllBooks()); dispatch(selezionaCategoria(null)) }}>
+                    <span className="iconcina w-7 sm:mx-2 mx-3 inline">
+                    </span>
+                    <span className="testoSide"> QUALSIASI </span>
+                </button>
+            </li>
                 { category.map((categoria, index) => (
                     <LeftBarList key={index} categoria={categoria} />
                 ))}
--- biblieria/src/Components/HomepageComp/LeftBarList.jsx
+++ biblieria/src/Components/HomepageComp/LeftBarList.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect , useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllBooks } from '../../slice/bookSlice';
-
-
+import "../../assets/css/Categories.css"
+import { selezionaCategoria } from '../../slice/bookSlice';
 
 export default function LeftBarList( {categoria} ) {
 
     const dispatch = useDispatch()
 
+    const categoriaSelezionata = useSelector(state => state.libri.categoriaSelezionata)
+
     return (
         <>
-            <li className="list text-center" key={categoria.id} >
-                <button className="flex gap-1 no-underline uppercase" onClick={() => { dispatch(getAllBooks({category:categoria.name})) }}>
+           
+            <li className={"list text-center " + categoriaSelezionata == categoria.id ? 'active' : ''} key={categoria.id} >
+                <button className="flex gap-1 no-underline uppercase "  onClick={() => { dispatch(getAllBooks({category_id: categoria.id})); dispatch(selezionaCategoria(categoria)) }}>
                     <span className="iconcina w-7 sm:mx-2 mx-3 inline">
                     </span>
                     <span className="testoSide"> {categoria.name} </span>
