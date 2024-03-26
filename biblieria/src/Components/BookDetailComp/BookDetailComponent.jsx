af5c98270fcf3d5d598c24f614e3ef10513b27c5
AleDC91 <ale91.dallas@gmail.com>
2024-03-26 01:00:31 +0100

    xfghxfg

--- biblieria/src/Components/BookDetailComp/BookDetailComponent.jsx
+++ biblieria/src/Components/BookDetailComp/BookDetailComponent.jsx
@@ -1,96 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../../api/axios'
-import { useNavigate, useParams } from 'react-router-dom'
-import { Image } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { Image } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import useAuthContext from "../../context/AuthContext";
-import { createBooking } from '../../slice/bookingSlice'
-import ModBook from '../ModBook'
-
+import { createBooking } from "../../slice/bookingSlice";
+import ModBook from "../ModBook";
 
 export default function BookDetailComponent() {
-    const {user} = useAuthContext();
-    
-    const mybooking = useSelector(state => state.bookings.listaBooking)
-    console.log(mybooking)
-   let {id}= useParams()
+  const { user } = useAuthContext();
 
-    let [book, setBook] = useState({})
-    let dispatch= useDispatch()
-   const navigate = useNavigate();
+  const mybooking = useSelector((state) => state.bookings.listaBooking);
+  console.log(mybooking);
+  let { id } = useParams();
 
-    useEffect(() => {
-        axios('/api/book/'+id).
-            then(response => {
-                console.log(response)    
-                setBook(response.data)
+  let [book, setBook] = useState({});
+  let dispatch = useDispatch();
+  const navigate = useNavigate();
 
-                }).
-            catch(err => console.log(err))
-        
-    },[])
+  useEffect(() => {
+    axios("/api/book/" + id)
+      .then((response) => {
+        console.log(response);
+        setBook(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        if(mybooking){
-        let alreadyBooked= mybooking.some(b => b.book_id == id)
-        console.log(alreadyBooked)
-        }
-}, [book])
-    
+  useEffect(() => {
+    if (mybooking) {
+      let alreadyBooked = mybooking.some((b) => b.book_id == id);
+      console.log(alreadyBooked);
+    }
+  }, [book]);
 
-                
-return (
-    book.author ? <> 
-    <div className="container bg-white rounded-2 p-5 my-5 font-sans">
+  //   update book
+
+
+  // end update book
+
+  return book.author ? (
+    <>
+      <div className="container bg-white rounded-2 p-5 my-5 font-sans">
         <div>
-           {user && user.is_admin ? 
-           (
-           <div>
-            <button className='btn btn-light w-75'>Elimina libro</button>
-            <ModBook/>
-           </div> 
-           )
-            :
-            ('')
-        }
+          {user && user.is_admin ? (
+            <div>
+              <button className="btn btn-light w-75">Elimina libro</button>
+              <ModBook  id={id}/>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         <div className="row">
-            <div className="col">
-                <div className='border-1 border-light rounded-1'>
-                    <Image src={book.cover_url}></Image>
-                </div>
+          <div className="col">
+            <div className="border-1 border-light rounded-1">
+              <Image src={book.cover_url}></Image>
             </div>
-            <div className="col">
-            
-                <h1 className='uppercase fs-2'>{book.title}</h1>
-                <h3 className='text-secondary'>
-                    <button className="flex no-underline"  onClick={()=> navigate(`/author/${book.author.id}`)}>
-                        <span className="testoSide"> {book.author.full_name} </span>
-                    </button>
-                </h3>
-                <div className='my-3'>
-                    <p className='m-0'><b>Anno pubblicazione: </b> <span className='text-secondary'>{book.year}</span></p>
-                    <p className='m-0'><b>Categoria: </b> <span className='text-secondary'>{book.category.name}</span></p>
-                    <p className='m-0'><b>Copie disponibili: </b> <span className='text-secondary'>{book.available_copies}</span></p>
-                </div>
-                <div className='my-4'>
-                   {book.abstract}
-                </div>
+          </div>
+          <div className="col">
+            <h1 className="uppercase fs-2">{book.title}</h1>
+            <h3 className="text-secondary">
+              <button
+                className="flex no-underline"
+                onClick={() => navigate(`/author/${book.author.id}`)}
+              >
+                <span className="testoSide"> {book.author.full_name} </span>
+              </button>
+            </h3>
+            <div className="my-3">
+              <p className="m-0">
+                <b>Anno pubblicazione: </b>{" "}
+                <span className="text-secondary">{book.year}</span>
+              </p>
+              <p className="m-0">
+                <b>Categoria: </b>{" "}
+                <span className="text-secondary">{book.category.name}</span>
+              </p>
+              <p className="m-0">
+                <b>Copie disponibili: </b>{" "}
+                <span className="text-secondary">{book.available_copies}</span>
+              </p>
             </div>
+            <div className="my-4">{book.abstract}</div>
+          </div>
         </div>
         <div className="row text-center mt-3">
-            <div className="col">
-                <button className='btn btn-dark w-75' onClick={()=> dispatch(createBooking(book.id))}>PRENOTA</button>
-            </div>
+          <div className="col">
+            <button
+              className="btn btn-dark w-75"
+              onClick={() => dispatch(createBooking(book.id))}
+            >
+              PRENOTA
+            </button>
+          </div>
         </div>
+      </div>
+    </>
+  ) : (
+    <div class="container bg-info p-5">
+      <h1>mettere gestione caricamento</h1>
     </div>
-    </> :
-    <div class='container bg-info p-5'>
-    <h1>mettere gestione caricamento</h1>
-</div>
-  )
-               
-                    
-                }
-  
-
+  );
+}
--- biblieria/src/Components/ModBook.jsx
+++ biblieria/src/Components/ModBook.jsx
@@ -1,40 +1,203 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
+import axios from "../api/axios";
+import { Form } from "react-bootstrap";
 
-export default function ModBook() {
+export default function ModBook({id}) {
+
+    const [bookData, setBookData] = useState({});
+
+    const getBookData = () => {
+      axios(`/api/book/${id}/edit`).then((res) => {
+        setBookData(res.data);
+   
+        console.log(res.data);
+      });
+    };
+  
+    useEffect(() => {
+      getBookData();
+    }, []);
+
+    useEffect(() => {
+        setTitle(bookData.book.title);
+        setAuthor(bookData.book.author_id);
+        setYear(bookData.book.year);
+        setAvailableCopies(bookData.book.available_copies);
+        setCoverUrl(bookData.book.cover_url);
+        setAbstract(bookData.book.abstract);
+        setCategory(bookData.book.category_id);
+        setCategories(bookData.categories);
+        setAuthors(bookData.authors);
+
+      }, [bookData]);
+
+  const [title, setTitle] = useState();
+  const [author_id, setAuthor] = useState();
+  const [year, setYear] = useState();
+  const [available_copies, setAvailableCopies] = useState();
+  const [cover_url, setCoverUrl] = useState();
+  const [abstract, setAbstract] = useState();
+  const [category_id, setCategory] = useState();
+
+  const [authors, setAuthors] = useState();
+  const [categories, setCategories] = useState();
+
+
+  const handleUpdateBook = () => {
+    axios.put(`api/book/${bookData.book.id}`, {
+      title,
+      author_id,
+      abstract,
+      year,
+      cover_url,
+      available_copies,
+      category_id,
+    });
+  };
+
   return (
-    <>  
-      
-        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <i class="bi bi-card-text"></i>
-        </button>
+     bookData.authors ?
+     <>
+      <button
+        type="button"
+        class="btn btn-dark"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        <i class="bi bi-card-text"></i>
+      </button>
 
-        
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
-            <div className="modal-content">
+          <div className="modal-content">
             <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Modifica Libro</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modifica Libro
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
-                <form action="">
-                    <input type="text" class="form-control my-1" name='title' placeholder='titolo libro' />
-                    <input type="number" class="form-control my-1" name='year' placeholder='anno' />
-                    <input type="number" class="form-control my-1" name='avalaible_copies' placeholder='copie disponibili' />
-                    <input type="text" class="form-control my-1" name='cover_url' placeholder='url immagine' />
-                    <input type="text" class="form-control my-1" name='abstract' placeholder='descrizione' />
-                    <input type="text" class="form-control my-1" name='category' placeholder='categoria' />
-                </form>
-            </div>
-            <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                <button type="button" className="btn btn-success">Salva modifiche</button>
-            </div>
-            </div>
-        </div>
-        </div>
-    </>
-  )
-}
+              <form onSubmit={handleUpdateBook}>
+                <input
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  type="text"
+                  class="form-control my-1"
+                  name="title"
+                  placeholder="titolo libro"
+                />
 
+                <Form.Select
+                  aria-label="Default select example"
+                  name="autore"
+                  onChange={(e) => setAuthor(e.target.value)}
+                  value={author_id}
+                >
+                  {bookData.authors.map((author) => {
+                    if (author.id === bookData.book.author.id) {
+                      return (
+                        <option
+                          key={author.id}
+                          selected="selected"
+                          value={author.id}
+                        >
+                          {author.full_name}
+                        </option>
+                      );
+                    } else {
+                      return (
+                        <option key={author.id} value={author.id}>
+                          {author.full_name}
+                        </option>
+                      );
+                    }
+                  })}
+                </Form.Select>
+
+                <input
+                  type="number"
+                  class="form-control my-1"
+                  name="year"
+                  placeholder="anno"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                />
+                <input
+                  type="number"
+                  class="form-control my-1"
+                  name="avalaible_copies"
+                  placeholder="copie disponibili"
+                  value={available_copies}
+                  onChange={(e) => setAvailableCopies(e.target.value)}
+                />
+                <input
+                  type="text"
+                  class="form-control my-1"
+                  name="cover_url"
+                  placeholder="url immagine"
+                  value={cover_url}
+                  onChange={(e) => setCoverUrl(e.target.value)}
+                />
+                <input
+                  type="text"
+                  class="form-control my-1"
+                  name="abstract"
+                  placeholder="descrizione"
+                  value={abstract}
+                  onChange={(e) => setAbstract(e.target.value)}
+                />
+
+                <Form.Select
+                  aria-label="Default select example"
+                  name="category"
+                  onChange={(e) => setCategory(e.target.value)}
+                >
+                  {bookData.categories.map((category) => {
+                    return (
+                      <option
+                        key={category.id}
+                        value={category.id}
+                        selected={
+                          category.id === bookData.book.category.id
+                            ? "selected"
+                            : ""
+                        }
+                      >
+                        {category.name}
+                      </option>
+                    );
+                  })}
+                </Form.Select>
+
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Chiudi
+                  </button>
+                  <button type="submit" className="btn btn-success">
+                    Salva modifiche
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </> : 'sdfgjmh'
+  );
+}
