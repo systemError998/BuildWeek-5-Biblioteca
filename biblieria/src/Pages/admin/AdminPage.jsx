30f283272b341729a8857f4baf593161cd960442
AleDC91 <ale91.dallas@gmail.com>
2024-03-26 13:56:35 +0100

    grafici

--- biblieria/src/Pages/admin/AdminPage.jsx
+++ biblieria/src/Pages/admin/AdminPage.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { AddUserForm } from "../../Components/AdminPageComp/AddUserForm";
 import { Link } from "react-router-dom";
 import AllUsersAdmin from "../../Components/AdminPageComp/AllUsersAdmin";
+import Chart from "./Chart";
+import PieChart from "./PieChart";
 
 export default function AdminPage() {
   return (
     <Container>
-      <div  className=" my-4">
-        <Link to={"/admin/createUser"} className=" btn btn-light bg-white ">
-          Add User
-        </Link>
-      </div>
-      <AllUsersAdmin />
+      <Row>
+        <Col>
+          <Link to={"/admin/createUser"} className=" btn btn-light bg-white my-4">
+            Add User
+          </Link>
+          <AllUsersAdmin />
+        </Col>
+        <Col>
+        <Row className="mt-5">
+          <Chart />
+        </Row>
+        <Row className="my-4">
+          <PieChart />
+        </Row>
+        </Col>
+      </Row>
     </Container>
   );
 }
